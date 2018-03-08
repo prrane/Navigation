82b4e22bb479d3c3ce72ba3fde601fe1a93b97ba
Prashant Rane <rane.p0@gmail.com>
2018-03-08 13:34:20 +0530

    Initial Commit

---
+++ Navigation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Prashant Rane on 08/03/18.
+//  Copyright © 2018 Prashant Rane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
